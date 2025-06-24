bd3b997275c22bfffd438f8f91648aa3f7c6179b
dailv1808 <ledai1808@gmail.com>
2025-06-24 11:57:11 +0000

    update

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -3,17 +3,25 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class UserManager(BaseUserManager):
+    def create_user(self, phone, password=None, **extra_fields):
+        if not phone:
+            raise ValueError('The Phone field must be set')
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
     def create_superuser(self, phone, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_admin', True)
         
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-        
-        return self._create_user(phone, password, **extra_fields)
+            
+        return self.create_user(phone, password, **extra_fields)
+
 
 class User(AbstractUser):
     phone = models.CharField(max_length=20, unique=True)
