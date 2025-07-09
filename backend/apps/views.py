4d232b50996684aa43b042cd5e550db98d7074cf
dailv1808 <ledai1808@gmail.com>
2025-07-09 14:55:31 +0000

    clean up

--- backend/apps/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-@api_view(['GET'])
-def hello(request):
-    return Response({'message': 'Hello from Django API!'})
--- backend/te.py
+++
@@ -1,5 +0,0 @@
-from django.db import connection
-cursor = connection.cursor()
-cursor.execute("SELECT 1;")
-print(cursor.fetchone())
-
