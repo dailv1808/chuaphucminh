3a9700ce0e136d6b86b6c0d709fa005ee1d828d3
dailv1808 <ledai1808@gmail.com>
2025-07-19 13:14:22 +0700

    change model qa

--- backend2/guest_question/admin.py
+++ backend2/guest_question/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
-from .models import GuestQuestion, QuestionGroup, QuestionTag
+from .models import GuestQuestion
 
 @admin.register(GuestQuestion)
 class GuestQuestionAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'email', 'short_content', 'status', 'priority', 'tags', 'group', 'answered_at', 'created_at')
     list_filter = ('status', 'priority', 'group', 'tags',)
     search_fields = ('name', 'email', 'content', 'short_content',)
-    autocomplete_fields = ['tags', 'group']
+    # autocomplete_fields = ['tags', 'group']
     readonly_fields = ['created_at', 'answered_at']
 
-@admin.register(QuestionGroup)
-class QuestionGroupAdmin(admin.ModelAdmin):
-    search_fields = ['name']
+# @admin.register(QuestionGroup)
+# class QuestionGroupAdmin(admin.ModelAdmin):
+#     search_fields = ['name']
 
-@admin.register(QuestionTag)
-class QuestionTagAdmin(admin.ModelAdmin):
-    search_fields = ['name']
+# @admin.register(QuestionTag)
+# class QuestionTagAdmin(admin.ModelAdmin):
+#     search_fields = ['name']
 
