956cfafdf391196b224082760d3ba76ecd2312e8
dailv1808 <ledai1808@gmail.com>
2025-07-19 12:01:41 +0700

    update qa page

--- backend2/guest_question/admin.py
+++ backend2/guest_question/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import GuestQuestion, QuestionGroup, QuestionTag
+from .models import GuestQuestion
 
 @admin.register(GuestQuestion)
 class GuestQuestionAdmin(admin.ModelAdmin):
