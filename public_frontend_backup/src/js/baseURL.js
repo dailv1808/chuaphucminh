bef7d381041eb73d0633a8e1451d5afe941db53d
dailv1808 <ledai1808@gmail.com>
2026-06-14 16:19:07 +0700

    remote backup public FE

--- public_frontend_backup/src/js/baseURL.js
+++
@@ -1,6 +0,0 @@
-function getApiUrl(endpoint) {
-//  const BASE_URL = 'http://192.168.0.200:8000';
-  const BASE_URL = 'https://api.chuaphucminh.top'; // Build PROD
-  return BASE_URL + endpoint;
-}
-
--- public_frontend_backup/src/js/main.js
+++
@@ -1,9 +0,0 @@
-// Import CSS toàn cục
-import '../css/style.css';       // CSS chung (Tailwind)
-
-// Import AlpineJS
-import Alpine from 'alpinejs';
-window.Alpine = Alpine;
-Alpine.start();
-
-// Import các file JS khác (nếu cần)
